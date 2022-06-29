a876f41070610b23d0e073276bfcc9eb2ffff0eb
Kai Middleton <kai.middleton@gmail.com>
2022-06-28 21:42:36 -0700

    Now works with ReactDOMServer.renderToString

--- node_server.js
+++ node_server.js
@@ -1,23 +1,30 @@
-const http = require('http')
-const port = 3000
+const http = require("http");
+const port = 3000;
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  let responseText = `
-  <h1>Hello Node.js Server!</h1>
-  <p>This is my own little html page, I hope you like it.</p>
-  <p>It is served by a very small node server</p>
-  `
-  response.end(responseText)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return React.createElement("div", null, "A very simple component");
+  }
 }
 
-const server = http.createServer(requestHandler)
+const appString = ReactDOMServer.renderToString(React.createElement(App, null));
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end(appString);
+};
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log("something bad happened", err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
-
+  console.log(`server is listening on ${port}`);
+});
